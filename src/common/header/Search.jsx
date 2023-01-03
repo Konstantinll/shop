f91ca535c2e29d4f715264610ccd72dbb99f65cc
Konstantinll <101046786+Konstantinll@users.noreply.github.com>
2023-01-03 15:03:39 +0300

    +link logo

--- src/common/header/Search.jsx
+++ src/common/header/Search.jsx
@@ -1,40 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../assets/logo.svg'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
 
-const Search = ({cartItem}) => {
-  window.addEventListener('scroll', function(){
-    const search = this.document.querySelector('.search')
-    search.classList.toggle('active', window.scrollY > 100)
-  })
+const Search = ({ cartItem }) => {
+  window.addEventListener("scroll", function () {
+    const search = this.document.querySelector(".search");
+    search.classList.toggle("active", window.scrollY > 100);
+  });
   return (
     <>
-        <section className="search">
-          <div className="container c_flex">
-            <div className="logo width">
-              <img src={logo} alt='logo'/>
-            </div>
-
-            <div className="search-box f-flex">
-              <i className="fa fa-search"></i>
-              <input type="text" placeholder='Search' />
-              <span>All Catagory</span>
-            </div>
-
-            <div className="icon f_flex width">
-              <i className="fa fa-user icon-circle"></i>
-              <div className="cart">
-                <Link to='/cart'>
-                  <i className="fa fa-shopping-bag icon-circle"></i>
-                  <span>{cartItem.length === 0 ? "" : cartItem.length}</span>
-                </Link>
-              </div>
-            </div>
+      <section className="search">
+        <div className="container c_flex">
+          <div className="logo width">
+            <Link to="/">
+              <img src={logo} alt="logo" />
+            </Link>
           </div>
 
-        </section>
-    </>
-  )
-}
+          <div className="search-box f-flex">
+            <i className="fa fa-search"></i>
+            <input type="text" placeholder="Search" />
+            <span>All Catagory</span>
+          </div>
 
-export default Search
+          <div className="icon f_flex width">
+            <i className="fa fa-user icon-circle"></i>
+            <div className="cart">
+              <Link to="/cart">
+                <i className="fa fa-shopping-bag icon-circle"></i>
+                <span>{cartItem.length === 0 ? "" : cartItem.length}</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Search;
